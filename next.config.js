2208459602d2294a0e5c00bbcdcbe5972bb8d215
bitf12m033 <umermj2@gmail.com>
2023-06-12 12:35:28 +0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {env: {
+    CONTENTFUL_SPACE_ID:'yg59vtf6z9oq',
+    CONTENTFUL_ENV:'master'
+}}
 
 module.exports = nextConfig
